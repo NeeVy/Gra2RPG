ef13ae9289defc686fd94fd64ee4b01f3f171d08
Unknown <delait7@gmail.com>
2017-04-10 14:07:20 +0200

    Add main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <cmath> 
+#include <string>
+#include <fstream> 
+#include <ctime> 
+#include <cstdlib>
+#include <iomanip> 
+#include <algorithm>
+#include "gameName.cpp"
+#include "grapostac.cpp"
+
+using namespace std;
+
+int main(){
+
+
+	return 0;
+}
+
